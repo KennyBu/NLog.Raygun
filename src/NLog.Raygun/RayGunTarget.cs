84ce6809e76b2a3ff9b8b2a97b55bf828dd4c0f3
Rolf Kristensen <sweaty1@hotmail.com>
2017-12-31 00:31:06 +0100

    Removed System.Web.Mvc dependency and included userCustomData

--- src/NLog.Raygun/RayGunTarget.cs
+++ src/NLog.Raygun/RayGunTarget.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web.Mvc;
 using Mindscape.Raygun4Net;
 using NLog.Config;
+using NLog.Layouts;
 using NLog.Targets;
 
 namespace NLog.Raygun
@@ -15,116 +13,192 @@ namespace NLog.Raygun
     [RequiredParameter]
     public string ApiKey { get; set; }
 
-    [RequiredParameter]
     public string Tags { get; set; }
 
-    [RequiredParameter]
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the Form data of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Form on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
     public string IgnoreFormFieldNames { get; set; }
 
-    [RequiredParameter]
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the cookies of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Cookies on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
     public string IgnoreCookieNames { get; set; }
 
-    [RequiredParameter]
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the server variables of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the ServerVariables on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
     public string IgnoreServerVariableNames { get; set; }
 
-    [RequiredParameter]
+    /// <summary>
+    /// Adds a list of keys to ignore when attaching the headers of an HTTP POST request. This allows
+    /// you to remove sensitive data from the transmitted copy of the Headers on the HttpRequest by specifying the keys you want removed.
+    /// This method is only effective in a web context.
+    /// </summary>
     public string IgnoreHeaderNames { get; set; }
 
-    [RequiredParameter]
+    /// <summary>
+    /// Specifies whether or not RawData from web requests is ignored when sending reports to Raygun.io.
+    /// The default is false which means RawData will be sent to Raygun.io.
+    /// </summary>
+    public bool IsRawDataIgnored { get { return _isRawDataIgnored ?? false; } set { _isRawDataIgnored = value; } }
+    private bool? _isRawDataIgnored;
+
+    /// <summary>
+    /// Explicitly defines lookup of user-identity for Raygun events.
+    /// </summary>
+    public Layout UserIdentityInfo { get; set; }
+
+    /// <summary>
+    /// Legacy parameter kept alive to avoid breaking change. NLog will not load configuration if trying to configure unknown properties
+    /// </summary>
+    [Obsolete("No longer supported. Instead consider using the UserIdentityInfo property")]
     public bool UseIdentityNameAsUserId { get; set; }
 
     /// <summary>
-    /// Attempt to get the executing assembly version, or root ASP.Net assembly version for Raygun events.
+    /// Attempt to get the entry assembly version
     /// </summary>
-    [RequiredParameter]
     public bool UseExecutingAssemblyVersion { get; set; }
 
     /// <summary>
     /// Explicitly defines an application version for Raygun events.
     /// NOTE: This value will be ignored if UseExecutingAssemblyVersion is set to true and returns a value.
     /// </summary>
-    public string ApplicationVersion { get; set; }
+    public string ApplicationVersion
+    {
+      get
+      {
+        SimpleLayout simpleLayout = _applicationVersion as SimpleLayout;
+        if (simpleLayout != null)
+          return simpleLayout.Text;
+        else if (_applicationVersion != null)
+          return _applicationVersion.ToString();
+        else
+          return null;
+      }
+      set { _applicationVersion = value; }
+    }
+    private Layout _applicationVersion;
+
+    private RaygunClient _raygunClient;
+
+    protected override void CloseTarget()
+    {
+      base.CloseTarget();
+      _raygunClient = null;
+    }
 
     protected override void Write(LogEventInfo logEvent)
     {
-      // If we have a real exception, we can log it as is, otherwise we can take the NLog message and use that.
-      if (IsException(logEvent))
+      _raygunClient = _raygunClient ?? (_raygunClient = CreateRaygunClient());
+
+      Exception exception = ExtractException(logEvent);
+      var tags = ExtractTags(logEvent, exception);
+      Dictionary<string, object> userCustomData = ExtractProperties(logEvent);
+      string layoutLogMessage = Layout.Render(logEvent);
+      userCustomData["RenderedLogMessage"] = layoutLogMessage;
+      userCustomData["LogMessageTemplate"] = logEvent.Message;
+
+      if (exception == null)
       {
-        Exception exception = (Exception)logEvent.Parameters.First();
-
-        List<string> tags = ExtractTagsFromException(exception);
-
-        RaygunClient raygunClient = CreateRaygunClient();
-        SendMessage(raygunClient, exception, tags);
-      }
-      else if (IsWebException(logEvent))
-      {
-        ExceptionContext exceptionContext = (ExceptionContext)logEvent.Parameters.First();
-        ProcessWebException(exceptionContext);
-      }
-      else
-      {
-        string logMessage = Layout.Render(logEvent);
-
-        RaygunException exception = new RaygunException(logMessage, logEvent.Exception);
-        RaygunClient client = CreateRaygunClient();
-
-        SendMessage(client, exception, new List<string>());
-      }
-    }
-
-    private static bool IsWebException(LogEventInfo logEvent)
-    {
-      return logEvent.Parameters.Any() && logEvent.Parameters.FirstOrDefault() != null && logEvent.Parameters.First().GetType() == typeof(ExceptionContext);
-    }
-
-    private static bool IsException(LogEventInfo logEvent)
-    {
-      return logEvent.Parameters.Any() && logEvent.Parameters.FirstOrDefault() != null && logEvent.Parameters.First().GetType() == typeof(Exception);
-    }
-
-    private void ProcessWebException(ExceptionContext exceptionContext)
-    {
-      Exception exception = exceptionContext.Exception;
-      List<string> tags = ExtractTagsFromException(exception);
-
-      RaygunClient raygunClient = CreateRaygunClient();
-
-      if (exceptionContext.HttpContext.Request.IsAuthenticated && UseIdentityNameAsUserId)
-      {
-        raygunClient.User = exceptionContext.HttpContext.User.Identity.Name;
+        exception = new RaygunException(layoutLogMessage);
       }
 
-      SendMessage(raygunClient, exception, tags);
+      string userIdentityInfo = UserIdentityInfo != null ? UserIdentityInfo.Render(logEvent) : string.Empty;
+      var userIdentity = string.IsNullOrEmpty(userIdentityInfo) ? null : new Mindscape.Raygun4Net.Messages.RaygunIdentifierMessage(userIdentityInfo);
+      _raygunClient.SendInBackground(exception, tags, userCustomData, userIdentity);
     }
 
-    private static List<string> ExtractTagsFromException(Exception exception)
+    private static Exception ExtractException(LogEventInfo logEvent)
     {
-      // Try and get tags off the exception data, if they exist
+      if (logEvent.Exception != null)
+      {
+        return logEvent.Exception;
+      }
+
+      if (logEvent.Parameters != null && logEvent.Parameters.Length > 0)
+      {
+        return logEvent.Parameters[0] as Exception;
+      }
+
+      return null;
+    }
+
+    private static Dictionary<string, object> ExtractProperties(LogEventInfo logEvent)
+    {
+      Dictionary<string, object> properties = new Dictionary<string, object>();
+      if (logEvent.Properties.Count > 0)
+      {
+        foreach (var property in logEvent.Properties)
+        {
+          string propertyKey = property.Key.ToString();
+          if (string.IsNullOrEmpty(propertyKey))
+            continue;
+
+          if (propertyKey == "Tags" || propertyKey == "tags")
+            continue;
+
+          object propertyValue = property.Value;
+          if (propertyValue == null || Convert.GetTypeCode(propertyValue) != TypeCode.Object || propertyValue is decimal)
+            properties[propertyKey] = propertyValue;
+          else
+            properties[propertyKey] = propertyValue.ToString();
+        }
+      }
+      return properties;
+    }
+
+    private List<string> ExtractTags(LogEventInfo logEvent, Exception exception)
+    {
       List<string> tags = new List<string>();
-      if (exception.Data["Tags"] != null)
+
+      // Try and get tags off the exception data, if they exist
+      if (exception != null && exception.Data != null)
       {
-        if (exception.Data["Tags"].GetType() == typeof(List<string>))
+        if (exception.Data.Contains("Tags"))
         {
-          tags.AddRange((List<string>)exception.Data["Tags"]);
+          object tagsData = exception.Data["Tags"];
+          tags.AddRange(ParseTagsData(tagsData));
         }
-
-        if (exception.Data["Tags"].GetType() == typeof(string[]))
+        if (exception.Data.Contains("tags"))
         {
-          tags.AddRange(((string[])exception.Data["Tags"]).ToList());
-        }
-
-        if (exception.Data["Tags"].GetType() == typeof(string))
-        {
-          tags.AddRange(SplitValues((string)exception.Data["Tags"]));
+          object tagsData = exception.Data["tags"];
+          tags.AddRange(ParseTagsData(tagsData));
         }
       }
+
+      if (logEvent.Properties.Count > 0)
+      {
+        if (logEvent.Properties.ContainsKey("Tags"))
+        {
+          object tagsData = logEvent.Properties["Tags"];
+          tags.AddRange(ParseTagsData(tagsData));
+        }
+        if (logEvent.Properties.ContainsKey("tags"))
+        {
+          object tagsData = logEvent.Properties["tags"];
+          tags.AddRange(ParseTagsData(tagsData));
+        }
+      }
+
+      if (!string.IsNullOrWhiteSpace(Tags))
+      {
+        var tagsData = SplitValues(Tags);
+        tags.AddRange(tagsData);
+      }
+
       return tags;
     }
 
     private RaygunClient CreateRaygunClient()
     {
-      var client = new RaygunClient(ApiKey);
+      var client = string.IsNullOrEmpty(ApiKey) ? new RaygunClient() : new RaygunClient(ApiKey);
 
       if (UseExecutingAssemblyVersion)
       {
@@ -133,63 +207,67 @@ namespace NLog.Raygun
 
       if (string.IsNullOrEmpty(client.ApplicationVersion))
       {
-        client.ApplicationVersion = ApplicationVersion;
-      }
-
-      client.IgnoreFormFieldNames(SplitValues(IgnoreFormFieldNames));
-      client.IgnoreCookieNames(SplitValues(IgnoreCookieNames));
-      client.IgnoreHeaderNames(SplitValues(IgnoreHeaderNames));
-      client.IgnoreServerVariableNames(SplitValues(IgnoreServerVariableNames));
-      return client;
-    }
-
-    private void SendMessage(RaygunClient client, Exception exception, IList<string> exceptionTags)
-    {
-      if (!string.IsNullOrWhiteSpace(Tags))
-      {
-        var tags = Tags.Split(',');
-
-        foreach (string tag in tags)
+        if (_applicationVersion != null)
         {
-          exceptionTags.Add(tag);
+          client.ApplicationVersion = _applicationVersion.Render(LogEventInfo.CreateNullEvent());
+        }
+        else
+        {
+          client.ApplicationVersion = GetExecutingAssemblyVersion();
         }
       }
 
-      client.SendInBackground(exception, exceptionTags);
+      if (IgnoreFormFieldNames != null)
+        client.IgnoreFormFieldNames(SplitValues(IgnoreFormFieldNames));
+      if (IgnoreCookieNames != null)
+        client.IgnoreCookieNames(SplitValues(IgnoreCookieNames));
+      if (IgnoreHeaderNames != null)
+        client.IgnoreHeaderNames(SplitValues(IgnoreHeaderNames));
+      if (IgnoreServerVariableNames != null)
+        client.IgnoreServerVariableNames(SplitValues(IgnoreServerVariableNames));
+      if (_isRawDataIgnored.HasValue)
+        client.IsRawDataIgnored = _isRawDataIgnored.Value;
+      return client;
+    }
+
+    private static IEnumerable<string> ParseTagsData(object tagsData)
+    {
+      IEnumerable<string> tagsCollection = tagsData as IEnumerable<string>;
+      if (tagsCollection != null)
+      {
+        return tagsCollection;
+      }
+      else if (tagsData is string)
+      {
+        return SplitValues((string)tagsData);
+      }
+      else
+      {
+        return System.Linq.Enumerable.Empty<string>();
+      }
     }
 
     private static string[] SplitValues(string input)
     {
       if (!string.IsNullOrWhiteSpace(input))
       {
-        return input.Split(',');
+        return input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
       }
 
-      return new[] { string.Empty };
+      return new string[0];
     }
 
     private static string GetExecutingAssemblyVersion()
     {
       try
       {
-        var assembly = Assembly.GetEntryAssembly();
-        if (assembly == null)
-        {
-          var type = System.Web.HttpContext.Current.ApplicationInstance.GetType();
-          while (type != null && type.Namespace == "ASP")
-          {
-            type = type.BaseType;
-          }
-          assembly = type != null ? type.Assembly : null;
-        }
+        var assembly = System.Reflection.Assembly.GetEntryAssembly();
         return assembly != null ? assembly.GetName().Version.ToString() : null;
-
       }
       catch (Exception)
       {
         return null;
       }
-
     }
   }
 }
